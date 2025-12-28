bae198bbf57aa316db112af66dca6d38cdc141ef
Zarek97 <kacperzarebski77@gmail.com>
2025-12-28 23:21:21 +0100

    Naprawa połączenia AI

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname)));
 
 const OPENROUTER_KEY = process.env.OPENROUTER_KEY;
